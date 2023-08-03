b8ac8ed291ac645505bc605ae3056bb351d46be2
Tri Luong <triluongdl@gmail.com>
2023-08-03 13:50:01 +0700

    edit config.

--- src/config/alpha.js
+++ src/config/alpha.js
@@ -3,6 +3,6 @@ const common = require("./common");
 module.exports = {
   ...common,
   REACT_APP_BUILD_ENV: "alpha",
-  BASE_URL: "https://dev-api.joininflow.com",
+  BASE_URL: "https://dev-api.joininflow.io",
   WEBSOCKET_BASE_URL: "wss://chat.portal.com",
 };
